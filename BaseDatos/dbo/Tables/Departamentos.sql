6dee09242f95137d9d0c25bc27d0a02c626c07fe
estebanqj84 <estebanqj1984@gmail.com>
2021-06-25 18:35:01 -0600

    tablas creadas

---
+++ BaseDatos/dbo/Tables/Departamentos.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE dbo.Departamentos
+(
+Id_Departamento INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_Departamento 
+PRIMARY KEY CLUSTERED(Id_Departamento)
+,Descripcion VARCHAR(250) NOT NULL
+ ,Ubicacion VARCHAR(250) NOT NULL
+,Estado BIT NOT NULL
+)
+WITH (DATA_COMPRESSION = PAGE)
+GO
