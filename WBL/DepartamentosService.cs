be8db3e628b79562a0353616988e40247312fd41
estebanqj84 <estebanqj1984@gmail.com>
2021-06-26 00:33:43 -0600

    entitys completos

---
+++ WBL/DepartamentosService.cs
@@ -0,0 +1,129 @@
+﻿using BD;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBL
+{
+    public class DepartamentosService
+    {
+        private readonly IDataAccess sql;
+
+        public DepartamentosService(IDataAccess _sql)
+        {
+            sql = _sql;
+        }
+
+        public async Task<IEnumerable<DepartamentosEntity>> Get()
+        {
+            try
+            {
+                var result = sql.QueryAsync<DepartamentosEntity>("DepartamentosObtener");
+
+                return await result;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+        }
+
+        public async Task<DepartamentosEntity> GetById(DepartamentosEntity entity)
+        {
+            try
+            {
+                var result = sql.QueryFirstAsync<DepartamentosEntity>("DepartamentosObtener", new
+                {
+                    entity.Id_Departamento
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+        }
+
+        public async Task<DBEntity> Create(DepartamentosEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("DepartamentosInsertar", new
+                {
+                    entity.Id_Departamento,
+                    entity.Descripcion,
+                    entity.Ubicacion,
+                    entity.Estado
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+        public async Task<DBEntity> Update(DepartamentosEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("DepartamentosActualizar", new
+                {
+                    entity.Id_Departamento,
+                    entity.Descripcion,
+                    entity.Ubicacion
+                    entity.Estado
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+
+
+        public async Task<DBEntity> Delete(DepartamentosEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("DepartamentoEliminar", new
+                {
+                    entity.Id_Departamento
+
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+    }
+}
---
+++ WBL/TitulosService.cs
@@ -0,0 +1,127 @@
+﻿using BD;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBL
+{
+    public class TitulosService
+    {
+        private readonly IDataAccess sql;
+
+        public TitulosService(IDataAccess _sql)
+        {
+            sql = _sql;
+        }
+
+        public async Task<IEnumerable<TitulosEntity>> Get()
+        {
+            try
+            {
+                var result = sql.QueryAsync<TitulosEntity>("TitulosObtener");
+
+                return await result;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+        }
+
+        public async Task<TitulosEntity> GetById(TitulosEntity entity)
+        {
+            try
+            {
+                var result = sql.QueryFirstAsync<TitulosEntity>("TitulosObtener", new
+                {
+                    entity.Id_Titulo
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+        }
+
+        public async Task<DBEntity> Create(TitulosEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("TitulosInsertar", new
+                {
+                    entity.Id_Titulo,
+                    entity.Descripcion,
+                    entity.Estado
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+        public async Task<DBEntity> Update(TitulosEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("TitulosActualizar", new
+                {
+                    entity.Id_Titulo,
+                    entity.Descripcion,
+                    entity.Estado
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+
+
+        public async Task<DBEntity> Delete(TitulosEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("TitulosEliminar", new
+                {
+                    entity.Id_Titulo
+
+                });
+
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+    }
+}
