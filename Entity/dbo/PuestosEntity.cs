8cc25f4b0e7b61fb99c063a7cb44e76fc0b5de45
estebanqj84 <estebanqj1984@gmail.com>
2021-06-25 22:39:16 -0600

    Creacion de Entity Puesto y Titulo

---
+++ Entity/dbo/PuestosEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class PuestosEntity: EN  //herencia
+    {
+        public int? Id_Puesto { get; set; }
+        public string Nombre { get; set; }
+        public int? Salario { get; set; }
+        public int? Estado { get; set; }
+    }
+}
